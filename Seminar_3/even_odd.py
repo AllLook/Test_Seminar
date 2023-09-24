079152e077e1f0914e6ce9789517afd7ddcb8d66
AllLook <alet5289@gmail.com>
2023-09-24 17:58:08 +0300

    Seminar3

---
+++ Seminar_3/even_odd.py
@@ -0,0 +1,14 @@
+def even_odd_num(num: int):
+    return num % 2 == 0
+
+
+def numberInInterval(num: int):
+    if num >= 25 and num <= 100:
+        return True
+    else:
+        return False
+
+
+# if __name__ == '__main__':
+#     print(even_odd_num(6))
+#     print(numberInInterval(3))
